b49fbf869708d7e5e9727b944dbe80109db22cd7
xykt <152045469+xykt@users.noreply.github.com>
2024-07-12 09:49:45 +0800

    Update noupgrade.sh

--- nezha/noupgrade.sh
+++ nezha/noupgrade.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+echo "提示: 此脚本将禁用nezha-agent自动升级功能。"
+read -p "是否继续? (y/n): " choice
+case "$choice" in 
+  y|Y ) 
+    echo "继续执行脚本..."
+    ;;
+  n|N ) 
+    echo "脚本已退出。"
+    exit 0
+    ;;
+  * ) 
+    echo "无效输入，脚本已退出。"
+    exit 1
+    ;;
+esac
 if command -v systemctl > /dev/null 2>&1; then
     systemctl stop nezha-agent
     sudo sed -i '/ExecStart=/ s/$/ --disable-auto-update/' /etc/systemd/system/nezha-agent.service
