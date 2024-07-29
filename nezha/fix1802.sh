31830eaa0328a5450976b4fd927c23936c868241
xykt <152045469+xykt@users.noreply.github.com>
2024-07-29 10:13:59 +0800

    Create fix1802.sh

---
+++ nezha/fix1802.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+echo "提示: 此脚本将升级/降级本机nezha-agent至v0.18.5版本（最后一个Websocket-WebShell版本，适配dashboard≤v0.18.2），并禁用自动升级功能。"
+read -p "是否继续? (y/n): " choice
+case "$choice" in 
+  y|Y ) 
+    echo "继续执行脚本..."
+    ;;
+  n|N ) 
+    echo "脚本已退出。"
+    exit 0
+    ;;
+  * ) 
+    echo "无效输入，脚本已退出。"
+    exit 1
+    ;;
+esac
+cd ~
+wget https://github.com/nezhahq/agent/releases/download/v0.18.5/nezha-agent_linux_amd64.zip
+unzip nezha-agent_linux_amd64.zip
+if command -v systemctl > /dev/null 2>&1; then
+    systemctl stop nezha-agent
+    cp nezha-agent /opt/nezha/agent
+    sudo sed -i '/ExecStart=/ s/$/ --disable-auto-update/' /etc/systemd/system/nezha-agent.service
+    systemctl daemon-reload
+    systemctl restart nezha-agent
+elif command -v rc-service > /dev/null 2>&1; then
+    rc-service nezha-agent stop
+    cp nezha-agent /opt/nezha/agent
+    sudo sed -i 's#command_args="\(.*\)"#command_args="\1 --disable-auto-update"#' /etc/init.d/nezha-agent
+    rc-update add nezha-agent
+    rc-service nezha-agent start
+else
+    echo "Unsupported init system"
+    exit 1
+fi
+rm nezha-agent_linux_amd64.zip
+rm nezha-agent
